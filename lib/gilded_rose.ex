4d52dd3f3bc163b069d83acd7ec56ce0a7cfeecf
Laura Cordoba <castelinolaura@gmail.com>
2020-08-25 12:09:00 -0300

    Refactor update_item code

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -7,77 +7,139 @@ defmodule GildedRose do
     Enum.map(items, &update_item/1)
   end
 
-  def update_item(item) do
-    item = cond do
-      item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-        if item.quality > 0 do
-          if item.name != "Sulfuras, Hand of Ragnaros" do
-            %{item | quality: item.quality - 1}
-          else
-            item
-          end
-        else
-          item
-        end
-      true ->
-        cond do
-          item.quality < 50 ->
-            item = %{item | quality: item.quality + 1}
-            cond do
-              item.name == "Backstage passes to a TAFKAL80ETC concert" ->
-                item = cond do
-                  item.sell_in < 11 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-                cond do
-                  item.sell_in < 6 ->
-                    cond do
-                      item.quality < 50 ->
-                        %{item | quality: item.quality + 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-              true -> item
-            end
-          true -> item
-        end
-    end
-    item = cond do
-      item.name != "Sulfuras, Hand of Ragnaros" ->
-        %{item | sell_in: item.sell_in - 1}
-      true -> item
-    end
-    cond do
-      item.sell_in < 0 ->
-        cond do
-          item.name != "Aged Brie" ->
-            cond do
-              item.name != "Backstage passes to a TAFKAL80ETC concert" ->
-                cond do
-                  item.quality > 0 ->
-                    cond do
-                      item.name != "Sulfuras, Hand of Ragnaros" ->
-                        %{item | quality: item.quality - 1}
-                      true -> item
-                    end
-                  true -> item
-                end
-              true -> %{item | quality: item.quality - item.quality}
-            end
-          true ->
-            cond do
-              item.quality < 50 ->
-                %{item | quality: item.quality + 1}
-              true -> item
-            end
-        end
-      true -> item
-    end
+  # Refactor of old code
+  def sell_in_less_zero(%Item{sell_in: sell_in} = item) when sell_in < 0 do
+    item
+    |> diff_aged()
   end
+
+  def sell_in_less_zero(item) do
+    item
+  end
+
+  def diff_aged(%Item{name: name} = item) when name != "Aged Brie" do
+    item
+    |> diff_backstate()
+  end
+
+  def diff_aged(item) do
+    item
+    |> max_quality()
+  end
+
+  def increase_quality(item) do
+    %{item | quality: item.quality + 1}
+  end
+
+  def max_quality(%Item{quality: quality} = item) when quality < 50 do
+    item
+    |> increase_quality()
+  end
+
+  def max_quality(item) do
+    item
+  end
+
+  def diff_backstate(%Item{name: name} = item) when name != "Backstage passes to a TAFKAL80ETC concert" do
+    item
+    |> quality_check()
+  end
+
+  def diff_backstate(item) do
+    item
+    |> reduce_item_quality()
+  end
+
+  def reduce_item_quality(item) do
+    %{item | quality: item.quality - item.quality}
+  end
+
+
+  def quality_check(%Item{quality: quality} = item) when quality > 0 do
+    item
+    |> diff_sulfuras_decrease_quality()
+  end
+
+  def quality_check(item) do
+    item
+  end
+
+  def reduce_item_sellin(item) do
+    %{item | sell_in: item.sell_in - 1}
+  end
+
+  def diff_sulfuras_sellin(%Item{name: name} = item) when name != "Sulfuras, Hand of Ragnaros" do
+    item
+    |> reduce_item_sellin()
+  end
+
+  def diff_sulfuras_sellin(%Item{name: name} = item) do
+    item
+  end
+
+  def sell_in_less_11(%Item{sell_in: sell_in} = item) when sell_in < 11 do
+    item
+    |> max_quality()
+  end
+
+  def sell_in_less_11(item) do
+    item
+  end
+
+  def sell_in_less_6(%Item{sell_in: sell_in} = item) when sell_in < 6 do
+    item
+    |> max_quality()
+  end
+
+  def sell_in_less_6(item) do
+    item
+  end
+
+  def diff_sulfuras_decrease_quality(%Item{name: name} = item) when name != "Sulfuras, Hand of Ragnaros" do
+    %{item | quality: item.quality - 1}
+  end
+
+  def diff_sulfuras_decrease_quality(item) do
+    item
+  end
+
+  def is_backstage(%Item{name: name} = item) when name == "Backstage passes to a TAFKAL80ETC concert" do
+    item
+    |> sell_in_less_11()
+    |> sell_in_less_6()
+  end
+
+  def is_backstage(item) do
+    item
+  end
+
+  def increase_quality_and_backstage(%Item{quality: quality} = item) when quality < 50 do
+    item
+    |> increase_quality()
+    |> is_backstage()
+  end
+
+  def increase_quality_and_backstage(item) do
+    item
+  end
+
+  def different_aged_backstage(%Item{name: name} = item) when name != "Aged Brie" and name != "Backstage passes to a TAFKAL80ETC concert" do
+    item
+    |> quality_check()
+  end
+
+  def different_aged_backstage(item)do
+    item
+    |> increase_quality_and_backstage()
+  end
+
+  # Rewrite old code for update_item
+  def update_item(item)  do
+    item
+    |> different_aged_backstage()
+    |> diff_sulfuras_sellin()
+    |> sell_in_less_zero()
+  end
+  # New Solution End
+
 end
